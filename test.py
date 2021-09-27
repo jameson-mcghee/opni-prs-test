5193a5bd09785c54b6a7b3c7be303a9cab9dc9ec
jamesonmcg <jameson.mcghee1@gmail.com>
2021-09-27 17:15:16 -0400

    Added Faker, removed test.log, and cleaning up code
    I added the Faker package, so that we could generate test payloads in code (allowing for the removal of the test.log file). Also, cleaning up some of the stuff such as how processes are initialized and killed and attempting to remove as many Wait statements as possible.

--- test.py
+++ test.py
@@ -7,11 +7,13 @@ from nats.aio.client import Client as NATS
 from nats.aio.errors import ErrConnectionClosed, ErrTimeout, ErrNoServers
 from queue import Queue
 from threading import Thread
+from faker import Faker
 
 from opni_nats import NatsWrapper
 
+from subprocess import Popen, PIPE
+
 kube_fname = "kube.yaml"
-logfile = "test.log"
 
 nw = NatsWrapper()
 
@@ -20,47 +22,65 @@ def test():
 
     start_process("killall kubectl")
 
-    log = open("test.log")
-    logdata = log.read()
-    log.close()
+    fake = Faker()
+    log_data = ('{"log": "' + fake.sentence(1000) + '"}')
 
-    k1 = start_process("kubectl --kubeconfig kube.yaml -n opni-demo port-forward svc/payload-receiver-service 8080:80 &")
-    k2 = start_process("kubectl --kubeconfig kube.yaml -n opni-demo port-forward svc/nats-client 4222:4222 &")
-
-    time.sleep(5)
+    port_forward_command = "kubectl --kubeconfig kube.yaml -n opni-demo port-forward "
+    kube_nats_process = Popen([port_forward_command + "svc/nats-client 4222:4222"], shell=True)
+    kube_payload_process = Popen([port_forward_command + "svc/payload-receiver-service 8080:80"], stdout=PIPE, shell=True)
+        
+    i = 0
+    while i <= 5 : 
+        if kube_payload_process.poll != None and kube_nats_process.poll != None :
+            time.sleep(1)
+            i += 1
+        else :
+            raise Exception('kube_payload_process and/or kube_nats_process are not running')
 
     # nats subscribe
     t = subscribe(tr_queue, "start worker process 32")
 
     wait_for_seconds(5)
 
-    print("sending data")
+    print("Sending dataset")
     r = requests.post("http://localhost:8080",
-                      data=logdata,
+                      data=log_data,
                       verify=False)
     
     if len(r.content) != 0:
-        print(r.content)
+        print(("Request content: "), r.content)
 
+    wait_for_seconds(2)
+    # i = 0
+    # while i <= 2 : 
+    #     if r.status_code != 200 :
+    #         time.sleep(1)
+    #         i += 1
+    #     else :
+    #         break
     assert r.status_code == 200
-
-    wait_for_seconds(5) 
 
     loop = asyncio.get_event_loop()
     loop.stop()
 
-    start_process("killall kubectl")
+    kube_payload_process.kill
+    kube_payload_process.wait
+    kube_nats_process.kill
+    kube_nats_process.wait
+    assert kube_payload_process.returncode == None
+    assert kube_nats_process.returncode == None
 
     foundlog = tr_queue.get()
     tr_queue.task_done()
     t.join()
 
+    print(("Foundlog: "), foundlog)
     assert foundlog == True
 
 
 def wait_for_seconds(seconds):
     start_time = time.time()
-    while time.time() - start_time < 5:
+    while time.time() - start_time < seconds:
         continue
 
 
@@ -72,7 +92,7 @@ def check_logs(incoming, expected):
 async def consume_logs(trqueue, logdata):
     async def subscribe_handler(msg):
         payload_data = msg.data.decode()
-        print(payload_data + '\n')
+        print('payload data' + payload_data + '\n')
         if check_logs(payload_data, logdata):
             trqueue.put(True)
 
@@ -85,7 +105,7 @@ async def consume_logs(trqueue, logdata):
 async def init_nats():
     print("Attempting to connect to NATS")
     await nw.connect()
-
+    assert nw.connect().__init__
 
 def start_background_loop(loop: asyncio.AbstractEventLoop) -> None:
     asyncio.set_event_loop(loop)
@@ -99,8 +119,8 @@ def subscribe(trqueue, logdata):
     t = Thread(target=start_background_loop, args=(loop,), daemon=True)
     t.start()
 
-    task = asyncio.run_coroutine_threadsafe(init_nats(), loop)
-    task = asyncio.run_coroutine_threadsafe(nats_consumer_coroutine, loop)
+    asyncio.run_coroutine_threadsafe(init_nats(), loop)
+    asyncio.run_coroutine_threadsafe(nats_consumer_coroutine, loop)
     
     return t
 
